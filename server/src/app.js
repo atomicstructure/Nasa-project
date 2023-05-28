570881b4ea4e7f010bce7c4691b5f3bd55b1727e
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2023-05-29 00:11:23 +0100

    Added loggin and installed morgan as the logger

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+const morgan = require('morgan')
+
 const app = express()
 
 const planetsRouter = require('./routes/planets/planets.router')
@@ -8,10 +10,17 @@ const planetsRouter = require('./routes/planets/planets.router')
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'))
 
 app.use(express.json());
-app.use(planetsRouter);
 app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+
+})
+
 
 
 
