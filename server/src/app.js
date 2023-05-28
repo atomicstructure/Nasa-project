aef28c5f4f5907047a383407ff2eebb874f459c8
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2023-05-28 23:30:12 +0100

    Serving the client from the server

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const app = express()
 
 const planetsRouter = require('./routes/planets/planets.router')
@@ -10,6 +11,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(planetsRouter);
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 
 
