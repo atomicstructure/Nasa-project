d995143dabdb788c8a617aac0ad8f376cd251703
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2023-05-28 21:44:00 +0100

    All dependencies added for now

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,40 @@
+const { parse } = require('csv-parse')
+const fs =require ('fs');
+const path = require ('path');
+
+const results = [];
+
+const habitablePlanet = function(planet){
+  return planet['koi_disposition'] === 'CONFIRMED'
+  && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+  && planet['koi_prad'] < 1.6
+}
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data','kepler_data.csv'))
+      .pipe(parse({
+         comment: '#',
+         columns: true,
+      }))
+      .on('data', (data) => {
+        if (habitablePlanet(data)){
+          results.push(data)
+         }
+        })
+        .on('error', (err) => {
+          console.log(err)
+          reject(err);
+        })
+        .on('end', () => {
+          console.log(`The total number of planets that are habitable is ${results.length}!`);
+          resolve();
+        })
+      });
+}
+
+
+  module.exports = {
+    loadPlanetsData,
+    planets: results,
+  };
