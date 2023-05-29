5ea9086b95f3af5ca89d1f7c78f79d77bf7151e9
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2023-05-29 14:06:10 +0100

    Accessed my API endpoints from POSTMAN and also made sure I am able to access other pages from index with /*

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 const app = express()
 
 const planetsRouter = require('./routes/planets/planets.router')
+const launchesRouter = require('./routes/launches/launches.router')
 
 app.use(cors({
   origin: 'http://localhost:3000'
@@ -15,8 +16,9 @@ app.use(morgan('combined'))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter);
+app.use(launchesRouter);
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 
 })
