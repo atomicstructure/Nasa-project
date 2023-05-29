838ac5d1027225abd7c986f54ebd1817c1d19f72
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2023-05-29 21:12:51 +0100

    Served data to rhe client

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -9,7 +9,18 @@ function httpGetAllLaunches(req, res){
 
 function httpAddNewLaunch(req, res){
   const launch = req.body;
+
+  if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination){
+    return res.status(400).json({
+      error: 'Please check you have all datas supplied!'
+    })
+  }
   launch.launchDate = new Date(launch.launchDate)
+  if(isNaN(launch.launchDate)){
+    return res.status(400).json({
+      error: 'Invalid launch date supplied'
+    });
+  }
   addNewlaunch(launch);
   return res.status(201).json(launch)
 }
