3d549f63caaf4d31a77f9da4e4702792fcdad1b7
Oluwafemi <ogundarearnold@gmail.com>
2023-06-17 07:21:52 +0100

    Abort function implemented

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,8 +3,6 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launches = new Map();
-
 
 const launch = {
   flightNumber: 100,
@@ -19,8 +17,10 @@ const launch = {
 
 saveLaunch(launch);
 
-function existsLaunchWithId(launchId){
-  return launches.has(launchId)
+async function existsLaunchWithId(launchId){
+  return await launchesDatabase.findOne({
+    flightNumber: launchId,
+  });
 }
 
 async function getLatestFlightNumber(){
@@ -72,11 +72,15 @@ async function scheduleNewLaunch(){
 }
 
 
-function abortLaunchById(launchId){
-  const aborted =launches.get(launchId);
-  aborted.upcoming = false;
-  aborted.success = false;
-  return aborted;
+async function abortLaunchById(launchId){
+  const aborted = await launchesDatabase.updateOne({
+    flightNumber: launchId,
+  }, {
+    upcoming: false,
+    success: false,
+  })
+
+  return aborted.ok === 1 && aborted.nModified === 1;
 }
 
 module.exports = {
